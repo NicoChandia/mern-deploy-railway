4ae8a534c244b2fc96a6609845efb1124e5f116b
NicoChandia <nicolas.chandia18@gmail.com>
2025-02-26 20:08:49 -0300

    arreglos en index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,4 +5,4 @@ import {PORT} from './config.js'
 //INDEX ARRANCA EL SERVIDOR
 
 app.listen(PORT)
-console.log(`Server running on http://locahost:${PORT}`)
+console.log(`Server running on http://localhost:${PORT}`)
