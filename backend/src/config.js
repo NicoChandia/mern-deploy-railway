5dd9b8e7217aa661780a5a6114a12d09f793da61
NicoChandia <nicolas.chandia18@gmail.com>
2025-02-26 19:56:21 -0300

    command start added

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,4 +1,4 @@
 console.log(process.env.MONGODB_URI)
-export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongodbtest'//'mongodb+srv://nicochan:12345@clustermongodbtest.sashi.mongodb.net/?retryWrites=true&w=majority&appName=ClusterMongodbtest'
-
+export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://nicochan:12345@clustermongodbtest.sashi.mongodb.net/?retryWrites=true&w=majority&appName=ClusterMongodbtest'
+//'mongodb://localhost/mongodbtest'
 export const PORT = process.env.PORT || 3000 //localmente 3000, deploy lee PORT
