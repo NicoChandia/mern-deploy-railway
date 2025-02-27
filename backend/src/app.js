f94f4af4c1d5a7e8c0afafc4b7dfe805478c5935
NicoChandia <nicolas.chandia18@gmail.com>
2025-02-26 21:02:16 -0300

    cambios en cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,11 @@ import cors from 'cors'
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://frontend-production-d2b9.up.railway.app',  // Reemplaza con la URL de tu frontend
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization'
+}))
 
 app.use(express.json())
 
@@ -13,16 +17,10 @@ app.get('/products', async (req, res) => {
     res.json(products)
 })
 
-app.post('/products', async (req, res) => { //CREAR productos
-    const {name, price, description} = req.body
-
-    const newProduct = await Product.create({
-        name,
-        price,
-        description
-    })
+app.post('/products', async (req, res) => {
+    const { name, price, description } = req.body
+    const newProduct = await Product.create({ name, price, description })
     res.json(newProduct)
 })
-
 
 export default app
